defc4c1f34266a194dd41d3315b62757ca46c3bf
Geoffrey Siele <sielekrisp@gmail.com>
2022-05-03 15:32:24 +0300

    generate 'contacts' resource and 'develop' branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/intouch'),
+    ContactsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
